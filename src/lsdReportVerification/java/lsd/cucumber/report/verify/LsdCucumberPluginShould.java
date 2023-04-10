e0b67bec20d91ddeb281c2154e1c3205e0d61472
nickmcdowall <nicholas.mcdowall@gmail.com>
2023-04-10 16:21:43 +0100

    fix: bump to lsd-core 3.4.0

--- src/lsdReportVerification/java/lsd/cucumber/report/verify/LsdCucumberPluginShould.java
+++ src/lsdReportVerification/java/lsd/cucumber/report/verify/LsdCucumberPluginShould.java
@@ -3,31 +3,40 @@ package lsd.cucumber.report.verify;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
-import static java.nio.charset.Charset.defaultCharset;
-import static java.nio.file.Files.readString;
 import static java.nio.file.Paths.get;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 class LsdCucumberPluginShould {
 
     @Test
     void generateMenu() throws IOException {
-        Approvals.verifyHtml(readString(get("build/reports/lsd/lsd-index.html"), defaultCharset()));
+        Approvals.verify(copyOfFile("build/reports/lsd/lsd-index.html"));
     }
 
     @Test
     void generateReportForBasicScenario() throws IOException {
-        Approvals.verifyHtml(readString(get("build/reports/lsd/Scenario.html"), defaultCharset()));
+        Approvals.verify(copyOfFile("build/reports/lsd/Scenario.html"));
     }
 
     @Test
     void generateReportForScenarioWithDataTable() throws IOException {
-        Approvals.verifyHtml(readString(get("build/reports/lsd/Data_table.html"), defaultCharset()));
+        Approvals.verify(copyOfFile("build/reports/lsd/Data_table.html"));
     }
 
     @Test
     void generateForScenarioOutline() throws IOException {
-        Approvals.verifyHtml(readString(get("build/reports/lsd/Scenario_outline.html"), defaultCharset()));
+        Approvals.verify(copyOfFile("build/reports/lsd/Scenario_outline.html"));
+    }
+
+    private File copyOfFile(String path) throws IOException {
+        Path original = get(path);
+        Path copy = Path.of(original.getParent().toString(), "copy_" + original.getFileName());
+        Files.copy(original, copy, REPLACE_EXISTING);
+        return copy.toFile();
     }
 }
